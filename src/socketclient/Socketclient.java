8573e3279fed6283fc28aacd05e9ff7d2241cc80
hasee <hasee@192.168.1.107>
2017-12-08 16:49:35 +0800

    break reconnect

--- src/socketclient/Socketclient.java
+++ src/socketclient/Socketclient.java
@@ -6,18 +6,56 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Socketclient {
 	public static final String IP_ADDR = "localhost";
 	public static final int PORT = 12345;
 
+	public static class Recnt implements Runnable {
+
+		private Socket socket;
+
+		public Recnt(Socket socket) {
+			this.socket = socket;
+		}
+
+		@Override
+		public void run() {
+			try {
+				socket.sendUrgentData(0xFF);
+			} catch (Exception ex) {
+				reconnect();
+			}
+		}
+
+	}
+
+	public static Socket reconnect() {
+		Socket socket = null;
+		while (true) {
+			try {
+				socket = new Socket(IP_ADDR, PORT);
+				break;
+			} catch (UnknownHostException e) {
+				continue;
+			} catch (IOException e) {
+				continue;
+			}
+		}
+		return socket;
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Starting...");
+
 		// System.out.println("当接收到服务器端字符为 \"Stop Server\" 的时候, 客户端将终止\n");
 		while (true) {
 			Socket socket = null;
 			try {
+
 				socket = new Socket(IP_ADDR, PORT);
+				Recnt rct = new Recnt(socket);
 
 				DataInputStream input = new DataInputStream(socket.getInputStream());
 				DataOutputStream out = new DataOutputStream(socket.getOutputStream());
@@ -28,6 +66,7 @@ public class Socketclient {
 				String ret = input.readUTF();
 				System.out.println("Return message from server: " + ret);
 				// 如接收到 "OK" 则断开连接
+
 				if ("Server:Stop Server\n".equals(ret)) {
 					System.out.println("Closing");
 					Thread.sleep(50);
